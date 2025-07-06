b3ba5ae5f86731ab11e044c51d83a8bc8d8cc549
jalalfaraj <farajenterprisesllc@gmail.com>
2025-07-06 13:23:21 -0400

    fixing sqlite3 compatibility issues

--- app.py
+++ app.py
@@ -14,6 +14,16 @@ from crewai import Agent, Task, Crew, LLM
 from chromadb import PersistentClient
 from chromadb.utils import embedding_functions
 from langchain_groq import ChatGroq
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # === LLM Setup ===
